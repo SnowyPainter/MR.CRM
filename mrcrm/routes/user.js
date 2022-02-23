cf47b2fe9f3cc7af236b25e1cb16ab0f6aab4321
Minwoo Yu <snowypainter@gmail.com>
2022-02-23 17:56:07 +0900

    User management implemented.

--- mrcrm/routes/user.js
+++ mrcrm/routes/user.js
@@ -11,16 +11,6 @@ router.get('/', auth.checkLogin, (req, res) => {
   })
 })
 
-function parsePermission(permission) {
-  let p = []
-  if(permission == null) return p;
-  permission.split(',').forEach(team => {
-    const s = team.trim().split(' ')
-    p.push([s[0], s[1]])
-  });
-  return p
-}
-
 router.get('/manage', auth.authIfNotRedirectLogin,(req, res) => {
   if (res.data.manager != 1) res.json({ err: "not manager" })
   
@@ -35,13 +25,38 @@ router.get('/manage', auth.authIfNotRedirectLogin,(req, res) => {
     }
   })
 })
-router.get('/update', (req, res) => {
+router.get('/update/:id', auth.authIfNotRedirectLogin, (req, res) => {
   if (res.data.manager != 1) res.json({ err: "not manager" })
+  const id = req.params.id;
+  const name = req.query.name;
+  const password = req.query.password;
+  const email = req.query.email;
+  const team = req.query.affteam;
+  const ismanager = req.query.ismanager;
+  let parsedPermission = [];
+  req.query.team.forEach((p) => parsedPermission.push(p))
+  parsedPermission = parsedPermission.join(', ');
+  res.db.update("User", {
+    "name": name,
+    "password": password,
+    "email": email,
+    "manager": ismanager,
+    "permission": parsedPermission
+  }, "WHERE id="+id);
 
-  console.log(req.query)
   res.redirect('/user/manage');
 })
 
+router.get('/get/team/:id', (req, res) => {
+  const id = req.params.id;
+  res.db.select("User", ["team"], "WHERE id="+id, (err, rows) => {
+    if(!err) {
+      res.json({data:rows})
+    } else {
+      res.json({data:{}})
+    }
+  })
+})
 
 router.post('/login', (req, res) => {
   res.db.select("User", [], "WHERE email='"+req.body.email+"' AND "+"password='"+req.body.password+"'", (err, rows) => {
@@ -52,7 +67,8 @@ router.post('/login', (req, res) => {
         email: row.email,
         name: row.name,
         manager: row.manager,
-        permission: parsePermission(row.permission)
+        permission: auth.parsePermission(row.permission),
+        team: row.team
       }
       let token = jwt.sign(user, config.secretCode);
       res.cookie("auth-token", token); 
